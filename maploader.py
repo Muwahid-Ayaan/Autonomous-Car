dbdbe6ba182e332be7c6754880e2fc5bee0ebaf3
Muwahid-Ayaan <sfmchoudry.ayaan@gmail.com>
2026-06-14 22:11:01 +0500

    Pure Pursuit to travel around map

---
+++ maploader.py
@@ -0,0 +1,10 @@
+import carla
+import time
+
+client = carla.Client('localhost', 2000)
+# Load a known CARLA map by name (avoid backslash escapes and relative paths)
+client.load_world('Town02')
+
+
+
+
